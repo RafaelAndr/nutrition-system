cf29c64bf6fe2effe9a5e247f7c13773e1eee4a3
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-25 12:44:37 -0300

    refactor(users): add more columns to entity

--- src/main/java/com/nutrition_system/entity/Users.java
+++ src/main/java/com/nutrition_system/entity/Users.java
@@ -1,8 +1,10 @@
 package com.nutrition_system.entity;
 
+import com.nutrition_system.enums.Role;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -14,12 +16,25 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(name = "username")
+    @Column(name = "username", nullable = false, unique = true, length = 100)
     private String username;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false, length = 200)
     private String password;
 
-    @Column(name = "role")
-    private String role;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
 }
