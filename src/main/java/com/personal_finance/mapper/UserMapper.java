6929109a19f29055dd4a4e949d580617006155c4
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-25 16:52:27 -0300

    feat(user): create user mapper

---
+++ src/main/java/com/personal_finance/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.personal_finance.mapper;
+
+import com.personal_finance.dto.resquest.UserRequestDto;
+import com.personal_finance.entity.Users;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    Users toEntity(UserRequestDto userRequestDto);
+}
