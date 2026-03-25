6d54de7c753be3e635b4ed4ee933d821539b330c
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-25 20:38:24 -0300

    refactor(user): change returns of crud methods

--- src/main/java/com/personal_finance/controller/UsersController.java
+++ src/main/java/com/personal_finance/controller/UsersController.java
@@ -1,6 +1,9 @@
 package com.personal_finance.controller;
 
+import com.personal_finance.dto.response.UserResponseDto;
+import com.personal_finance.dto.resquest.UserRequestDto;
 import com.personal_finance.entity.Users;
+import com.personal_finance.mapper.UserMapper;
 import com.personal_finance.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,28 +19,34 @@ import java.util.UUID;
 public class UsersController {
 
     private final UsersService usersService;
+    private final UserMapper userMapper;
 
     @PostMapping
-    public ResponseEntity<Users> create(@RequestBody Users user){
-        Users userCreated = usersService.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    public ResponseEntity<UserResponseDto> create(@RequestBody UserRequestDto userRequestDto){
+        Users userCreated = usersService.save(userMapper.toEntity(userRequestDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.toDto(userCreated));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Users> getById(@PathVariable UUID id){
+    public ResponseEntity<UserResponseDto> getById(@PathVariable UUID id){
         Users user = usersService.searchById(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Users> updatePassword(@PathVariable UUID id, @RequestBody Users user){
+    public ResponseEntity<UserResponseDto> updatePassword(@PathVariable UUID id, @RequestBody Users user){
         Users userToUpdatePassword = usersService.editPassword(id, user.getPassword());
-        return ResponseEntity.ok(userToUpdatePassword);
+        return ResponseEntity.ok(userMapper.toDto(userToUpdatePassword));
     }
 
     @GetMapping
-    public ResponseEntity<List<Users>> getAll(){
+    public ResponseEntity<List<UserResponseDto>> getAll(){
         List<Users> users = usersService.findAll();
-        return ResponseEntity.ok(users);
+
+        List<UserResponseDto> listResponseDto = users.stream()
+                .map(userMapper::toDto)
+                .toList();
+
+        return ResponseEntity.ok(listResponseDto);
     }
 }
--- src/main/java/com/personal_finance/service/UsersService.java
+++ src/main/java/com/personal_finance/service/UsersService.java
@@ -1,6 +1,9 @@
 package com.personal_finance.service;
 
+import com.personal_finance.dto.response.UserResponseDto;
+import com.personal_finance.dto.resquest.UserRequestDto;
 import com.personal_finance.entity.Users;
+import com.personal_finance.mapper.UserMapper;
 import com.personal_finance.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,8 +17,9 @@ import java.util.UUID;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final UserMapper userMapper;
 
-    @Transactional
+    @Transactional //só precisa usar transaction quando há mais de uma operação no metodo
     public Users save(Users user) {
         return usersRepository.save(user);
     }
