2d3e98630a0bf247cc69a06dcd6010ef22dfcf99
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-26 21:32:45 -0300

    feat(security): create class JwtToken

---
+++ src/main/java/com/personal_finance/security/JwtToken.java
@@ -0,0 +1,14 @@
+package com.personal_finance.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtToken {
+    private String password;
+}
