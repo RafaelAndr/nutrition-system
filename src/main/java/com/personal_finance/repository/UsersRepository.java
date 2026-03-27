31cdff377229deabc3ee1809e29315d12d00809b
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-26 21:35:15 -0300

    feat(user): create user repository

--- src/main/java/com/personal_finance/repository/UsersRepository.java
+++ src/main/java/com/personal_finance/repository/UsersRepository.java
@@ -1,11 +1,16 @@
 package com.personal_finance.repository;
 
 import com.personal_finance.entity.Users;
+import com.personal_finance.entity.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsersRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByUsername(String username);
+
+    @Query("select u.role from Users u where u.username like :username")
+    Role findRoleByUsername(String username);
 }
