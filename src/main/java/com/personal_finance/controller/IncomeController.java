ad25bf50dad51b7bd24f73b7e25d020b592758c6
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-04-08 12:11:15 -0300

    feat(income): add routes

--- src/main/java/com/personal_finance/controller/IncomeController.java
+++ src/main/java/com/personal_finance/controller/IncomeController.java
@@ -2,12 +2,15 @@ package com.personal_finance.controller;
 
 import com.personal_finance.dto.income.IncomeRequestDto;
 import com.personal_finance.dto.income.IncomeResponseDto;
+import com.personal_finance.entity.Income;
 import com.personal_finance.service.IncomeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/incomes")
@@ -17,7 +20,22 @@ public class IncomeController {
     private final IncomeService incomeService;
 
     @PostMapping
-    public IncomeResponseDto createIncome(@RequestBody IncomeRequestDto incomeRequestDto){
-        return incomeService.save(incomeRequestDto);
+    public ResponseEntity<IncomeResponseDto> createIncome(@RequestBody IncomeRequestDto incomeRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(incomeService.save(incomeRequestDto));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<IncomeResponseDto> getIncome(@PathVariable UUID id){
+        return ResponseEntity.ok(incomeService.getIncome(id));
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<IncomeResponseDto>> getUserIncomes(){
+        return ResponseEntity.ok(incomeService.getAllUserIncome());
+    }
+
+    @GetMapping("/accounts/{accountId}")
+    public ResponseEntity<List<IncomeResponseDto>> getAccountIncomes(@PathVariable UUID accountId){
+        return ResponseEntity.ok(incomeService.getAllAccountIncome(accountId));
     }
 }
