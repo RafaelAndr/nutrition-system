4ed0fc422f5569039635b966a8a3c713539156fd
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-04-23 10:52:39 -0300

    test(authentication): implement unit tests

---
+++ src/test/java/com/personal_finance/service/AuthenticationServiceTest.java
@@ -0,0 +1,64 @@
+package com.personal_finance.service;
+
+import com.personal_finance.dto.user.LoginUserDto;
+import com.personal_finance.security.JwtService;
+import com.personal_finance.security.JwtToken;
+import com.personal_finance.security.JwtUserDetails;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthenticationServiceTest {
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private JwtService jwtService;
+    @Mock
+    private JwtUserDetails jwtUserDetails;
+    @Mock
+    private Authentication authentication;
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    @Test
+    void shouldReturnJwtToken_WhenCredentialsAreValid(){
+        LoginUserDto dto = new LoginUserDto("rafael", "123");
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+
+        when(authentication.getPrincipal()).thenReturn(jwtUserDetails);
+
+        when(jwtService.generateToken(jwtUserDetails)).thenReturn("fake-token");
+
+        JwtToken result = authenticationService.login(dto);
+
+        assertThat(result.token()).isEqualTo("fake-token");
+
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(jwtService).generateToken(jwtUserDetails);
+    }
+
+    @Test
+    void shouldThrowException_WhenCredentialsAreInvalid(){
+        LoginUserDto dto = new LoginUserDto("rafael", "wrong");
+
+        when(authenticationManager.authenticate(any()))
+                .thenThrow(new BadCredentialsException("Invalid credentials"));
+
+        assertThatThrownBy(() -> authenticationService.login(dto))
+                .isInstanceOf(BadCredentialsException.class);
+    }
+}
